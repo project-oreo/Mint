159a14988119568c2bf16dbc1c491a46172f6179
jeffreyzarate1 <jeffreyzarate1@gmail.com>
2019-05-18 16:00:37 -0400

    added band and gig controllers

---
+++ Crescendo/src/main/java/com/mint/services/GigService.java
@@ -0,0 +1,36 @@
+package com.mint.services;
+import javax.inject.Inject;
+
+import org.springframework.stereotype.Service;
+import com.mint.entities.Gig;
+import com.mint.repositories.GigRepository;
+
+@Service
+public class GigService {
+
+	GigRepository gigRepository;
+	
+	@Inject
+	public GigService(GigRepository gigRepository)
+	{
+		super();
+		this.gigRepository = gigRepository;
+	}
+	
+	public Gig getById(int id) {
+		return this.gigRepository.getById(id);
+	}
+
+	public Gig create(Gig gig) {
+		return this.gigRepository.create(gig);
+	}
+
+	public Gig update(Gig gig) {
+		return this.gigRepository.update(gig);
+	}
+
+	public Gig deleteById(int id) {
+		return this.gigRepository.deleteById(id);
+	}
+
+}
