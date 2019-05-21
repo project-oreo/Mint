b5851357d848028af8021edc25e34aa089c9f39f
Brandon Slade <brandon.t.slade@gmail.com>
2019-05-21 12:05:12 -0400

    Brandon Slade May 21, 2019 @12:05 PM
    Pushing changes made prior to lunch.

---
+++ Crescendo/Crescendo-Client-side/src/app/components/promoter/promotermaster/promotermaster.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-promotermaster',
+  templateUrl: './promotermaster.component.html',
+  styleUrls: ['./promotermaster.component.css']
+})
+export class PromotermasterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
