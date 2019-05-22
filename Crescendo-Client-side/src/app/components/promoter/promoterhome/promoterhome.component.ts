ce7aa50d963e65f7bae70eb54a2b9ffbafc7cb08
jeffreyzarate1 <jeffreyzarate1@gmail.com>
2019-05-22 13:35:31 -0400

    prototype

--- Crescendo-Client-side/src/app/components/promoter/promoterhome/promoterhome.component.ts
+++ Crescendo-Client-side/src/app/components/promoter/promoterhome/promoterhome.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-promoterhome',
   templateUrl: './promoterhome.component.html',
   styleUrls: ['./promoterhome.component.css']
 })
+
 export class PromoterhomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private modalRef: BsModalRef, private modalService: BsModalService) { }
 
   ngOnInit() {
   }
+  openGig(template: TemplateRef<any>) {
+      console.log(template);
+      this.modalRef = this.modalService.show(template);
+  }
 
+  openBand(template: TemplateRef<any>) {
+    console.log(template);
+    this.modalRef = this.modalService.show(template);
+}
 }
