a5569b6bc71642455a99b4bd93fe425f2932a5c4
Abslan Laverde <BattlestarGalactica@107-144-156-227.biz.spectrum.com>
2019-05-25 15:48:29 -0400

    Abslan Laverde 05/25/2019 3:48 PM
    Configured front end to work with updated backend.  No major changes needed.  Just some testing.  Update gigs fully functional.

--- Crescendo-Client-side/src/app/services/promoterhome.service.ts
+++ Crescendo-Client-side/src/app/services/promoterhome.service.ts
@@ -87,7 +87,7 @@ export class PromoterhomeService {
 
     console.log(payload);
 
-    this.httpClient.put('http://ec2-52-15-213-35.us-east-2.compute.amazonaws.com:8081/Crescendo/gigs', payload, {
+    this.httpClient.put('http://ec2-52-15-213-35.us-east-2.compute.amazonaws.com:8081/Crescendo/gigs/updateGig', payload, {
       observe: 'response'
     })
     .subscribe(response => {
