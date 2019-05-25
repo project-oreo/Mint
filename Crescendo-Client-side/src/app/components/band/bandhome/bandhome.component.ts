1457566649102eb163f8e2800256b5470f0c97f0
Brandon Slade <brandon.t.slade@gmail.com>
2019-05-25 01:59:05 -0400

    Brandon Slade May 25, 2019 @2:00 AM
    finished the functionality for accepting and denying gig invites.

--- Crescendo-Client-side/src/app/components/band/bandhome/bandhome.component.ts
+++ Crescendo-Client-side/src/app/components/band/bandhome/bandhome.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { Gig } from 'src/app/classes/gig';
 import { BandloginService } from 'src/app/services/bandlogin.service';
-import { HomeService } from 'src/app/services/home.service';
 
 @Component({
   selector: 'app-bandhome',
@@ -17,14 +16,27 @@ export class BandhomeComponent implements OnInit {
   lastStatus = 200;
   vid: any;
 
-  constructor(private bandloginService: BandloginService, private homeService: HomeService) { }
+  constructor(private bandloginService: BandloginService) { }
 
   ngOnInit() {
     this.assignedGigs = this.bandloginService.assignedGigs;
     this.invitedGigs = this.bandloginService.invitedGigs;
+    console.log(this.invitedGigs);
   }
 
   clearStorage() {
     localStorage.clear();
+  }
+
+  Approved(id: number) {
+    localStorage.setItem('status', 'Approved');
+    localStorage.setItem('gigId', id.toString());
+    this.bandloginService.Approve();
+  }
+
+  Denied(id: number) {
+    localStorage.setItem('status', 'Denied');
+    localStorage.setItem('gigId', id.toString());
+    this.bandloginService.Approve();
   }
 }
