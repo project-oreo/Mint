c0a1442962126b3a02d41a114276e75443286063
Brandon Slade <brandon.t.slade@gmail.com>
2019-05-25 15:48:47 -0400

    Brandon Slade May 25, 2019 @3:48 PM
    added the ec2 address abck in to the servce.

--- Crescendo-Client-side/src/app/services/edit-profile.service.ts
+++ Crescendo-Client-side/src/app/services/edit-profile.service.ts
@@ -26,7 +26,7 @@ export class EditProfileService {
     hashedPassword: hashedPassword
   };
 
-    this.httpClient.put('http://localhost:8081/Crescendo/bands/update', payload, {
+    this.httpClient.put('http://ec2-52-15-213-35.us-east-2.compute.amazonaws.com:8081/Crescendo/bands/update', payload, {
     observe: 'response'}).subscribe(response => {
       this.editStatusSubject.next(200);
     }, err => {
